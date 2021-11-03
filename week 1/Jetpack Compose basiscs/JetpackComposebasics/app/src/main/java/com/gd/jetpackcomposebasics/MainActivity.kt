1a2811760c84a137ced296ef57ec3e4cec25e184
Dev-Czle <czle22@gmail.com>
2021-11-03 23:03:47 +0900

    JetpackCompose 첫주의 절반 완료 (6번까지)

--- week 1/Jetpack Compose basiscs/JetpackComposebasics/app/src/main/java/com/gd/jetpackcomposebasics/MainActivity.kt
+++ week 1/Jetpack Compose basiscs/JetpackComposebasics/app/src/main/java/com/gd/jetpackcomposebasics/MainActivity.kt
@@ -1,27 +1,20 @@
 package com.gd.jetpackcomposebasics
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.gd.jetpackcomposebasics.model.SampleData
 import com.gd.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,74 +22,61 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeBasicsTheme {
-                Conversation(messages = SampleData.conversationSample)
+                MyApp()
             }
         }
     }
 }
 
-data class Message(val author: String, val body: String)
-
 @Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(8.dp)) {
-        Image(
-            painter = painterResource(id = R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
-        )
+fun Greeting(name: String) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    // val expanded = remember{ mutableStateOf(false) }
 
-        Spacer(modifier = Modifier.width(8.dp))
+    val extraPadding = if (expanded) 48.dp else 0.dp
 
-        var isExpanded by remember {
-            mutableStateOf(false)
-        }
-
-        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colors.secondaryVariant,
-                style = MaterialTheme.typography.subtitle2
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Surface(
-                shape = MaterialTheme.shapes.medium,
-                elevation = 1.dp
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
+                Text(text = "Hello")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { expanded = !expanded }) {
                 Text(
-                    text = msg.body,
-                    style = MaterialTheme.typography.body2,
-                    modifier = Modifier.padding(4.dp),
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                    if (expanded) {
+                        "show Less"
+                    } else {
+                        "show More"
+                    }
                 )
             }
         }
+
     }
 }
 
+@Preview(showBackground = true, widthDp = 320)
 @Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn {
-        items(messages) { message ->
-            MessageCard(msg = message)
-        }
-    }
-}
-
-@Preview(name = "Light Mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)
-
-@Composable
-fun PreviewMessageCard() {
+fun DefaultPreview() {
     JetpackComposeBasicsTheme {
-        MessageCard(Message("Colleague", "Hey take a look at Jetpack Compose"))
+        MyApp()
+    }
+}
+
+@Composable
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
