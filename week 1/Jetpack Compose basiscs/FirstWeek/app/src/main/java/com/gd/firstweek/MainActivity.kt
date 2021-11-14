90336852300709c9e94f91fd2164f885b22d8e23
Dev-Czle <czle22@gmail.com>
2021-11-14 20:45:35 +0900

    FirstWeek 끝

--- week 1/Jetpack Compose basiscs/FirstWeek/app/src/main/java/com/gd/firstweek/MainActivity.kt
+++ week 1/Jetpack Compose basiscs/FirstWeek/app/src/main/java/com/gd/firstweek/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.gd.firstweek
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gd.firstweek.ui.theme.FirstweekTheme
@@ -30,40 +38,106 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    var expanded by remember {
-        mutableStateOf(false)
-    }
-    // val expanded = remember{ mutableStateOf(false) }
+//    var expanded by remember {
+//        mutableStateOf(false)
+//    }
+//    // val expanded = remember{ mutableStateOf(false) }
+//
+//    val extraPadding by animateDpAsState(
+//        if (expanded) 48.dp else 0.dp,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow
+//        )
+//    )
+//
+//    Surface(
+//        color = MaterialTheme.colors.primary,
+//        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+//    ) {
+//        Row(modifier = Modifier.padding(24.dp)) {
+//            Column(
+//                modifier = Modifier
+//                    .weight(1f)
+//                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+//            ) {
+//                Text(text = "Hello")
+//                Text(text = name)
+//            }
+//            OutlinedButton(onClick = { expanded = !expanded }) {
+//                Text(
+//                    if (expanded) {
+//                        "show Less"
+//                    } else {
+//                        "show More"
+//                    }
+//                )
+//            }
+//        }
+//    }
 
-    val extraPadding = if (expanded) 48.dp else 0.dp
-
-    Surface(
-        color = MaterialTheme.colors.primary,
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello")
-                Text(text = name)
-            }
-            OutlinedButton(onClick = { expanded = !expanded }) {
-                Text(
-                    if (expanded) {
-                        "show Less"
-                    } else {
-                        "show More"
-                    }
-                )
-            }
-        }
-
+        CardContent(name)
     }
 }
 
+@Composable
+fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name)
+        }
+    }
+//    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+//        for (name in names) {
+//            Greeting(name = name)
+//        }
+//    }
+}
+
+@Composable
+fun MyApp() {
+    var shouldShowOnboarding by rememberSaveable {
+        mutableStateOf(true)
+    }
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = {
+                    onContinueClicked()
+                }
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
@@ -72,11 +146,57 @@ fun DefaultPreview() {
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-private fun MyApp(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
+fun OnboardingPreview() {
+    FirstweekTheme() {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
         }
     }
 }
